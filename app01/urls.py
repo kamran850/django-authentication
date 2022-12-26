ac012a88cb3d7f2cd20498d874ea48e48f1cf902
swesadiqul <mdsadiqulislam390@gmail.com>
2022-12-26 09:46:41 +0600

    change user password

--- app01/urls.py
+++ app01/urls.py
@@ -5,9 +5,10 @@ from .views import*
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path("books/", books, name="books"),
-    path("register", signup, name="signup"),
-    path("profile", profile, name="profile"),
-    path("signout", signout, name="signout"),
-    path("signin", signin, name="signin"),
+    path("register", UserRegister, name="signup"),
+    path("profile", UserProfile, name="profile"),
+    path("signout", UserLogout, name="signout"),
+    path("signin", UserLogin, name="signin"),
+    path('password', UserChangePassword, name='change_password'),
     # path("books/signout/", signout, name="logout"),
 ]
--- app01/views.py
+++ app01/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 from .forms import *
 from django.views.generic import TemplateView, CreateView
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+
 
 # # Create your views here
 class HomeView(TemplateView):
@@ -17,8 +19,7 @@ def books(request):
     else:
         return redirect('/books/signin')
 
-def signup(request):
-    form = UserCreationForm()
+def UserRegister(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -35,13 +36,14 @@ def signup(request):
             return render(request, 'auth/signup.html', {'form':form})
 
     else:
-        return render(request, 'auth/signup.html', {'form':form})
+        form = UserCreationForm()
+    return render(request, 'auth/signup.html', {'form':form})
 
 @login_required
-def profile(request):
+def UserProfile(request):
     return render(request, 'auth/profile.html')
 
-def signin(request):
+def UserLogin(request):
     form = AuthenticationForm()
     if request.method == 'POST':
         username = request.POST['username']
@@ -52,30 +54,30 @@ def signin(request):
             messages.success(request, 'User successfully signin.')
             return redirect('profile')
         else:
-            form = AuthenticationForm()
+            messages.warning(request, 'Invalid username or password.')
             return render(request, 'auth/signin.html', {'form':form})
 
     else:
         form = AuthenticationForm()
-        return render(request, 'auth/signin.html', {'form':form})
+    return render(request, 'auth/signin.html', {'form':form})
 
 @login_required
-def signout(request):
+def UserLogout(request):
     logout(request)
     messages.info(request, 'User successfully logout.')
     return redirect('home')
 
-# from django.shortcuts import render
-# from django.contrib.auth.decorators import login_required
- 
-# def login(request):
-#     return render(request, 'login.html')
- 
-# @login_required
-# def home(request):
-#     return render(request, 'home.html')
 
-# class SignUp(CreateView):
-#     form_class = SignUpForm
-#     template_name = 'auth/user_form.html'
-#     success_url = '/login/'
+def UserChangePassword(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('signin')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'auth/change_password.html', {'form': form})
